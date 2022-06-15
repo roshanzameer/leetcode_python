0488d9a8cfc490865155637e7b02ebb022f34d37
Roshan Zameer <rossi.zameer@gmail.com>
2022-06-15 21:09:33 +0530

    Time Complexity notations

--- easy/longest_common_prefix.py
+++ easy/longest_common_prefix.py
@@ -16,17 +16,21 @@
 def longestCommonPrefix(strs) -> str:
 
     res = ""
-    word = min(strs, key=len)
+    word = min(strs, key=len)       # O(n)
     
-    for i in range(len(word)):
+    for i in range(len(word)):      # O(m)
         
-        for j in strs:
+        for j in strs:              # O(n)
                 
             if j[i] != word[i]:
                 return res
-        res += word[i]
+        res += word[i]              # O(m)  A new string is built each time
             
     return res
 
 
 print(longestCommonPrefix(["flower","flow","flight"]))
+
+
+
+# Complexity: O( n  +  m*n  +  m*m)
