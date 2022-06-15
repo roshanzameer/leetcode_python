192b2a885ed8a9b3058c8416cfd26479f9775ea3
Roshan Zameer <rossi.zameer@gmail.com>
2022-06-16 00:01:33 +0530

    Valid Paranthesis

---
+++ easy/valid_paranthesis.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/valid-parentheses/
+
+#Example 1:
+
+#Input: s = "()"
+#Output: true
+#Example 2:
+
+#Input: s = "()[]{}"
+#Output: true
+
+
+def isValid(s: str) -> bool:
+	
+	record = [] 
+	
+	pairs = {"}": "{", "]": "[", ")": "("}   
+	
+	for sym in s:	# O(n)
+
+		if sym == '{' or sym == '[' or sym == '(':
+			record.append(sym)    # O(1)
+		else:
+			if pairs[sym] == record[-1]:   
+				record.pop()    # O(1)
+
+	return len(record) == 0 
+	
+	
+print(isValid("{}[]([])"))
+
+
+
+# Time Complexity O(n)
