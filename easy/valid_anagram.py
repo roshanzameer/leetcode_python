56ae43c7694a64a1b82fe5b376b1126f68e1d5a1
Roshan Zameer <rossi.zameer@gmail.com>
2022-06-29 14:44:54 +0530

    Valid Anagram

--- easy/valid_anagram.py
+++ easy/valid_anagram.py
@@ -10,15 +10,13 @@ def isAnagram(s, t):
         count_s[s[i]] = 1 + count_s.get(s[i], 0)
         count_t[t[i]] = 1 + count_t.get(t[i], 0)
         
-        print(count_s)
-        print(count_t)
 
     for el in count_s:
-        print(count_s.get(el), count_t.get(el))
         if count_s[el] != count_t.get(el, 0):
             return False
         
     return True
 
+    #return count_s == count_t  # Also works
 
 print(isAnagram('anagram', 'nagaram'))
