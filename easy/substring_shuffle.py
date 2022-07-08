b19b146140f06ae01701ea181dcb6a1db6a10ae0
Roshan Zameer <rossi.zameer@gmail.com>
2022-07-08 16:23:05 +0530

    shuffled substring

---
+++ easy/substring_shuffle.py
@@ -0,0 +1,24 @@
+
+def is_substring():
+    str1 = "onetwofour"
+    str2 = "hellofourtwooneworld"
+
+    res = 0
+    i = 0
+    j = len(str1)
+    while i < len(str2):
+        substring = str2[i:j]
+        if sorted(str1) == sorted(substring):
+            res += 1
+        i+=1
+        j+=1
+
+    return res
+
+print(is_substring())
+
+
+
+
+
+
